6eda6ce906d739e8e57d1cbe2074763947d8bad9
Legwarmer1481 <70u3nulj@larknight.anonaddy.com>
2022-10-16 12:59:46 -0400

    Add AI to ennemy

--- Assets/Scripts/Ennemis.cs
+++ Assets/Scripts/Ennemis.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class Ennemis : MonoBehaviour
 {
     [Header("Cible")]
     [SerializeField] GameObject cible;
 
+    [Header("Caracteristiques")]
+    [SerializeField] float vitesse = 2f;
+
+    // Composants
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    // Evenements
+    public UnityEventTouche;
+
     void Start(){
 
+        if (cible == null)
+        {
+            cible = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        agent = GetComponent<NavMeshAgent>();
+        //agent.updateRotation = false;
+        //agent.updateUpAxis = false;
+        agent.speed = vitesse;
     }
 
     void Update(){
-
+        agent.SetDestination(cible.transform.position);
     }
 }
