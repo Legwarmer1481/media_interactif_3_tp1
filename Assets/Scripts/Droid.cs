d3e665064674e9f7131a8a4fb8fb56ab5e24f8d2
Legwarmer1481 <70u3nulj@larknight.anonaddy.com>
2022-10-19 09:15:28 -0400

    Add droid

--- Assets/Scripts/Droid.cs
+++ Assets/Scripts/Droid.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Droid : MonoBehaviour
 {
+    private int nbDestinations;
+    private int destiIndex = 0;
+    private Vector3 direction = Vector3.zero;
+    [SerializeField] Vector3[] destinations;
+    [SerializeField] float ralentissement = 1f;
+    [SerializeField] float interval = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        nbDestinations = destinations.Length;
+        Invoke("ChangerDirection", interval);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Bouge();
+    }
+
+    void Bouge(){
+
+        Debug.Log("ok");
+
+        transform.position = Vector3.SmoothDamp(transform.position, destinations[destiIndex], ref direction, ralentissement);
+
+    }
+
+    void ChangerDirection(){
+
+        if(destiIndex == nbDestinations - 1){
+            destiIndex = 0;
+        }else{
+            destiIndex++;
+        }
+
+        Invoke("ChangerDirection", interval);
     }
 }
